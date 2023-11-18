12d7e70bf0420ad3e3f14435fa642f47625761c5
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-11-18 20:14:00 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -4,7 +4,11 @@
 #echo " what is your name "
 #read person
 #echo " what is the $person"
-name="sivalingam"
-readonly name
-name="sivalingam the great"
-echo " $name "
+#name="sivalingam"
+#readonly name
+#name="sivalingam the great"
+#echo " $name "
+a=10
+if [ $a -eq 10 ]; then
+  echo "this is the true"
+fi
