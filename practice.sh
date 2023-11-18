24a7df15449f610e6500ff05fd831248ca6e49bd
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-11-18 20:35:23 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -8,9 +8,21 @@
 #readonly name
 #name="sivalingam the great"
 #echo " $name "
-a="siva"
-if [ ""${a} == "siv" ]; then
-  echo "this is the true"
-  else
-    echo "this is the false"
-fi
+#a="siva"
+#if [ ""${a} == "siv" ]; then
+#  echo "this is the true"
+#  else
+#    echo "this is the false"
+#fi
+a=0
+while [ $a -lt 10 ]    # this is loop1
+do
+   b=$a
+   while [ $b -ge 0 ]  # this is loop2
+   do
+      echo -n $b
+      b="expr $b - 1"
+   done
+   echo
+   a="expr $a + 1"
+done
