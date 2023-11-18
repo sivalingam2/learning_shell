7543e46cdf71f3f08f28f77511cb18d1a8c2f7fa
SIVA LINGAM <sivalingamganaganboyina@gmail.com>
2023-11-18 17:45:54 +0530

    practice

--- practice.sh
+++ practice.sh
@@ -1,6 +1,10 @@
 # a=10
 # echo " $a "
 # echo $a
-echo " what is your name "
-read person
-echo " what is the $person"
+#echo " what is your name "
+#read person
+#echo " what is the $person"
+name="sivalingam"
+readonly name
+name="sivalingam the great"
+echo " $name "
